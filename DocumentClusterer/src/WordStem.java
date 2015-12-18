5b6ad38381660e7850c810d2e83509a463ca03be
vibhutis1995 <vibhuti.s1995@gmail.com>
2015-12-18 13:02:43 +0530

    Create WordStem.java

---
+++ DocumentClusterer/src/WordStem.java
@@ -0,0 +1,17 @@
+public class WordStem {
+	private String word;
+	private String stem;
+	
+	public WordStem(String word, String stem){
+		this.word = word;
+		this.stem = stem;
+	}
+	
+	public String getWord(){
+		return word;
+	}
+	
+	public String getStem(){
+		return stem;
+	}
+}
