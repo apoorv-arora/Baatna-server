efc4bf92442cc05b147f34fc7db56e288ded1a59
vibhutis1995 <vibhuti.s1995@gmail.com>
2015-12-18 13:03:08 +0530

    Update NeedItem.java

--- DocumentClusterer/src/NeedItem.java
+++ DocumentClusterer/src/NeedItem.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class NeedItem {
 
-	public static String filepath = "C:/Copy of DocumentClusterer/src/Resources/example";
+	public static String filepath = "C:/DocumentClusterer/src/Resources/example";
 	
 	public static Item buildItem(String fileurl)
 	{
