99bcd8e7a9d435ed6c26a4a09c888a2e00359113
baatna <android@baatna.com>
2016-01-16 14:32:37 +0530

    fixes

--- BaatnaServer/src/com/application/baatna/resource/AppConfig.java
+++ BaatnaServer/src/com/application/baatna/resource/AppConfig.java
@@ -28,7 +28,7 @@ public class AppConfig {
 	@Produces("application/json")
 	@Consumes("application/x-www-form-urlencoded")
 	public JSONObject appUpdate(@FormParam("client_id") String clientId, @FormParam("app_type") String appType,
-			@FormParam("app_version") String version,@FormParam("access_token") String accessToken) {
+			@FormParam("app_version") String version, @FormParam("access_token") String accessToken) {
 		boolean clientTypeAndroid = false;
 		// check for client_id
 		if (!clientId.equals(CommonLib.ANDROID_CLIENT_ID)) {
@@ -41,54 +41,31 @@ public class AppConfig {
 			clientTypeAndroid = false;
 		} else
 			clientTypeAndroid = true;
-		try{
-		JSONObject appJsonObject= new JSONObject();
+		try {
+			JSONObject appJsonObject = new JSONObject();
 
-		if (clientTypeAndroid) {
-			if (CommonLib.ANDROID_APP_VERSION.equals(version))
-			{
-				appJsonObject.put("update", true);
-			}
-			else
-				appJsonObject.put("update", false);
-			
-			UserDAO dao= new UserDAO();
-			int userId=dao.userActive(accessToken);
-			
-			ArrayList<User> list= null;
-			if (userId > 0) {
-				
-				JSONArray jsonArr=dao.usersToBeRated(userId);
-				appJsonObject.put("rating_list", jsonArr);
-				
-				   /* list=dao.usersToBeRated(userId);
-					JSONArray jsonArr = null;
-					if(list.isEmpty())
-						appJsonObject.put("rating_list", jsonArr);
-					else
-					{
-						jsonArr=new JSONArray();
-					for (User user: list) {
-						
-						jsonArr.put(JsonUtil.getUserJson(user));
-					}
+			if (clientTypeAndroid) {
+				if (CommonLib.ANDROID_APP_VERSION.equals(version)) {
+					appJsonObject.put("update", true);
+				} else
+					appJsonObject.put("update", false);
+
+				UserDAO dao = new UserDAO();
+				int userId = dao.userActive(accessToken);
+
+				if (userId > 0) {
+
+					JSONArray jsonArr = dao.usersToBeRated(userId);
 					appJsonObject.put("rating_list", jsonArr);
-					}*/
-			  
-			return CommonLib.getResponseString(appJsonObject, "", CommonLib.RESPONSE_SUCCESS);
-			}
-			else
+
+					return CommonLib.getResponseString(appJsonObject, "", CommonLib.RESPONSE_SUCCESS);
+				} else
+					return CommonLib.getResponseString("Invalid params", "", CommonLib.RESPONSE_FAILURE);
+
+			} else
 				return CommonLib.getResponseString("Invalid params", "", CommonLib.RESPONSE_FAILURE);
-				
-				//return CommonLib.getResponseString(true, "", CommonLib.RESPONSE_SUCCESS);
-			//else
-				//return CommonLib.getResponseString(false, "", CommonLib.RESPONSE_SUCCESS);
-		} 
-		else
-			return CommonLib.getResponseString("Invalid params", "", CommonLib.RESPONSE_FAILURE);
-		}catch(JSONException e)
-		{
-		e.printStackTrace();	
+		} catch (JSONException e) {
+			e.printStackTrace();
 		}
 		return null;
 	}
